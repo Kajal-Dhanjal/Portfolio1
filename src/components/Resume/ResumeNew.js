d030080635ec008770dc296afa0a7b5a35c7bcaa
Bree Kagwe <kagwe.bridgette@gmail.com>
2026-05-24 19:06:05 +1000

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,6 +8,12 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+
+// EDIT THIS PART:
+// The import above on line 5 where it says Placeholder_CV.pdf, change it to your resume file name.
+// Do not change anything else on that line.
+
+
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
